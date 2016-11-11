a22c973b597596b2762a36de3e597d41dd51d8da
Nick Englund <englundn@gmail.com>
2016-11-11 10:32:59 -0800

    fix package.json

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// This file configures webpack to run on the production files
+const path = require('path');
+const webpack = require('webpack');
+const page = require('./page.json');
+
+module.exports = {
+  cache: true,
+  entry: page,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'js/[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+};
