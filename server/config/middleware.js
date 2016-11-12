454a9b8e7d043aa72a91c3bd5e0411065f8df339
Frédéric Renken <renken@users.noreply.github.com>
2016-11-12 11:48:16 -0800

    added cors headers

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,6 +2,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 module.exports = (app, express) => {
+  app.use((req, res, next) => {
+    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('access-control-allow-headers', 'content-type, accept');
+    res.header('access-control-max-age', 10);
+    next();
+  });
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
   app.use(express.static(path.join(__dirname, '/../../dist/')));
