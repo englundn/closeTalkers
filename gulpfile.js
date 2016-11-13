8708fd73a5afb2119818b03e6fc7841cf7295411
Frédéric Renken <renken@users.noreply.github.com>
2016-11-12 16:36:48 -0800

    added potato salad favicon

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ const gulp = require('gulp');
 const del = require('del');
 
 gulp.task('copy', () => {
-  const folders = ['src/**/*.html', 'src/**/*.css', 'src/lib/**/*', 'src/**/*.png'];
+  const folders = ['src/**/*.html', 'src/**/*.css', 'src/lib/**/*', 'src/**/*.png', 'src/favicon/*'];
   gulp.src(folders[0])
-      .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'));
 
   gulp.src(folders[1])
-      .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'));
 
   gulp.src(folders[2])
-      .pipe(gulp.dest('./dist/lib'));
+    .pipe(gulp.dest('./dist/lib'));
 
   gulp.src(folders[3])
-      .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'));
+
+  gulp.src(folders[4])
+    .pipe(gulp.dest('./dist/favicon'));
 });
 
 gulp.task('clean', () =>
