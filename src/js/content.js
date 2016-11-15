4ee4f1a1d0a883eff65c4af3214669d75c42f9a7
Frédéric Renken <renken@users.noreply.github.com>
2016-11-15 14:03:54 -0800

    Add link to url and fix some styling

--- src/js/content.js
+++ src/js/content.js
@@ -5,7 +5,7 @@ const Content = ({ result }) => (
     <div className="contentHeader">
       <span className="contentTitle">{result._source.title}</span>
       <span className="contentTimestamp">{new Date(result._source.timestamp).toDateString()}</span>
-      <span className="contentUrl">{result._source.url}</span>
+      <span className="contentUrl"><a href={result._source.url}>{result._source.url}</a></span>
     </div>
     <div className="contentBody">
       {result._source.text.split('\n').map((para, i) => (
