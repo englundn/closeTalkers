4bf7a8cabf7fefa8a7883e7900e7b49ea06074e2
Frédéric Renken <renken@users.noreply.github.com>
2016-11-15 13:57:20 -0800

    Display url and timestamp

--- src/js/content.js
+++ src/js/content.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Content = ({ result }) => (
   <div className="content">
     <div className="contentHeader">
-      <p>{result._source.title}</p>
+      <span className="contentTitle">{result._source.title}</span>
+      <span className="contentTimestamp">{new Date(result._source.timestamp).toDateString()}</span>
+      <span className="contentUrl">{result._source.url}</span>
     </div>
     <div className="contentBody">
       {result._source.text.split('\n').map((para, i) => (
