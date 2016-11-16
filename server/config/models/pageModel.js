7d84253702595467809c9c17a9dbd479d90dc1f1
Jace <chesszhu@gmail.com>
2016-11-15 18:52:55 -0800

    (feat) integrated checksum to ultra fast duplicate entry detection

--- server/config/models/pageModel.js
+++ server/config/models/pageModel.js
@@ -1,57 +1,71 @@
-const rp = require('request-promise');
-
+const request = require('request-promise');
 const user = process.env.ELASTIC_USER || require('../config').user;
 const pass = process.env.ELASTIC_PASSWORD || require('../config').password;
+const checksum = require('checksum');
 
 const URL = 'http://1b4f84fecd657bad91626e9aa8f74e59.us-west-1.aws.found.io:9200';
 
-module.exports = {
-  create: (url, title, userId, text) => {
-    const options = {
-      method: 'POST',
-      uri: `${URL}/${userId}/archive`,
-      auth: {
-        user,
-        pass,
-      },
-      body: {
-        url,
-        title,
-        timestamp: Date.now(),
-        text,
-      },
-      json: true,
-    };
-
-    rp(options)
-      .catch((err) => {
-        console.error(err);
-      });
-  },
-  search: (qs, id, callback) => {
-    const options = {
+const search = (id, qs, cs) => {
+  if (cs) {
+    return {
       method: 'GET',
       uri: `${URL}/${id}/archive/_search`,
-      auth: {
-        user,
-        pass,
-      },
+      auth: { user, pass },
       body: {
         query: {
           match: {
-            text: qs,
+            cs,
           },
         },
       },
       json: true,
     };
+  }
+  return {
+    method: 'GET',
+    uri: `${URL}/${id}/archive/_search`,
+    auth: { user, pass },
+    body: {
+      query: {
+        match: {
+          text: qs,
+        },
+      },
+    },
+    json: true,
+  };
+};
 
-    rp(options)
+const createOptions = (url, title, id, text) => {
+  const timestamp = Date.now();
+  const cs = checksum(text);
+  return {
+    method: 'POST',
+    uri: `${URL}/${id}/archive`,
+    auth: { user, pass },
+    body: { url, title, timestamp, text, cs },
+    json: true,
+  };
+};
+
+// ============= CREATE FROM EXTENSION ============
+module.exports = {
+  create: (url, title, id, text) => {
+    // checks if data exists by comparing cs
+    request(search(id, null, checksum(text)))
       .then((data) => {
-        callback(data);
+        if (data.hits.total === 0) {
+          request(createOptions(url, title, id, text))
+            .catch(err => console.error(err));
+        }
       })
-      .catch((err) => {
-        console.error(err);
-      });
+      .catch(() => console.log('error'));
+  },
+
+// =========== SEARCH FROM WEBSITE =============
+  search: (qs, id, callback) => {
+    request(search(id, qs, null))
+      .then(data => callback(data))
+      .catch(err => console.error(err));
   },
 };
