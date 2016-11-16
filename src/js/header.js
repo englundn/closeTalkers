b69d38c6f297f35fe24bae284c2d6b43540b19d3
Frédéric Renken <renken@users.noreply.github.com>
2016-11-15 17:11:52 -0800

    Styled header with login and home button

--- src/js/header.js
+++ src/js/header.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const Header = ({ query, queryString, handleChange }) => (
   <div className="header">
-    <form className="searchBar" onSubmit={query}>
-      <input
-        className="inputField" type="text"
-        placeholder="Search"
-        value={queryString}
-        onChange={handleChange}
-      />
-    </form>
-    <a href="/login">Login</a>
+    <div className="homeLink">
+      <a href="/#">Déjà Vu</a>
+    </div>
+    <div className="searchBar" >
+      <form onSubmit={query}>
+        <input
+          className="inputField" type="text"
+          placeholder="Search"
+          value={queryString}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+    <div className="loginLink">
+      <a href="/login">Log In</a>
+    </div>
   </div>
 );
 
