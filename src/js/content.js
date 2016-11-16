54b28ed21be1e1f1f98637331e525fe4a7e4ecba
Nick Englund <englundn@gmail.com>
2016-11-16 11:27:47 -0800

    Added timeAgo styling for content

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,45 @@
 import React from 'react';
 
+const timeSince = (date) => {
+  const seconds = Math.floor((new Date() - date) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return `${interval} years ago`;
+  }
+  if (interval === 1) {
+    return `${interval} year ago`;
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return `${interval} months ago`;
+  }
+  if (interval === 1) {
+    return `${interval} month ago`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return `${interval} days ago`;
+  }
+  if (interval === 1) {
+    return `${interval} day ago`;
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return `${interval} hours ago`;
+  }
+  if (interval === 1) {
+    return `${interval} hour ago`;
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return `${interval} minutes ago`;
+  }
+  if (interval === 1) {
+    return `${interval} minute ago`;
+  }
+  return `${Math.floor(seconds)} seconds ago`;
+};
+
 const Content = ({ result }) => (
   <div className="content">
     <div className="contentHeader">
@@ -7,7 +47,7 @@ const Content = ({ result }) => (
         {result._source.title}
       </span>
       <span className="contentTimestamp">
-        {new Date(result._source.timestamp).toDateString()}
+        {timeSince(result._source.timestamp)}
       </span>
       <span className="contentUrl">
         <a href={result._source.url}>{result._source.url}</a>
