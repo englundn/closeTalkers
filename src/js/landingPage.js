2297fe2831f70b4fd19425c059824e04beeb1f9e
Frédéric Renken <renken@users.noreply.github.com>
2016-11-15 20:24:46 -0800

    Add basic landing page heading and subheading

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const LandingPage = () => (
-  <div className="landingHeader">
-    <div className="loginLink">
-      <a href="/login">Log In</a>
+  <div>
+    <div className="landingHeader">
+      <div className="loginLink">
+        <a href="/login">Log In</a>
+      </div>
+    </div>
+    <div className="landingMain">
+      <h1>Déjà Vu</h1>
+      <h2>Find anything you have ever browsed</h2>
     </div>
   </div>
 );
