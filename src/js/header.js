de9299521ad963b1c7ef193447ed29770936b3aa
Frédéric Renken <renken@users.noreply.github.com>
2016-11-17 17:25:41 -0800

    Comment out homeLink until needed

--- src/js/header.js
+++ src/js/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ query, handleChange, isLoggedIn }) => (
   <div className="header">
     <div className="homeLink">
-      <a href="/?#">Déjà Vu</a>
+      <a href="/?#">{/* Déjà Vu */}</a>
     </div>
     <div className="searchBar" >
       {isLoggedIn === true &&
