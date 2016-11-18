334987ec1866b4f673f1ae079d7a9d9130fb2447
Frédéric Renken <renken@users.noreply.github.com>
2016-11-17 19:05:20 -0800

    Add chrome webstore install function and checking for installed app

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const LandingPage = () => (
-  <div className="landingMain">
-    <h1>Déjà Vu</h1>
-    <h2>Find anything you have ever browsed</h2>
-    <img className="webStore" src="../img/chrome-web-store.png" alt="" />
-  </div>
-);
+class LandingPage extends React.Component {
+  installExtension() {
+    chrome.webstore.install();
+  }
+
+  render() {
+    return (
+      <div className="landingMain">
+        <h1>Déjà Vu</h1>
+        <h2>Find anything you have ever browsed</h2>
+        {!chrome.app.isInstalled &&
+          <img className="webStore" onClick={this.installExtension.bind(this)} src="../img/chrome-web-store.png" alt="" />
+        }
+      </div>
+    );
+  }
+}
 
 export default LandingPage;
