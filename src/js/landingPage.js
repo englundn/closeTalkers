5a843fbbd29b0acdfc6bd95c7a44b6efdb61c336
Frédéric Renken <renken@users.noreply.github.com>
2016-11-17 17:34:48 -0800

    Add chrome web store img

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -4,6 +4,7 @@ const LandingPage = () => (
   <div className="landingMain">
     <h1>Déjà Vu</h1>
     <h2>Find anything you have ever browsed</h2>
+    <img className="webStore" src="../img/chrome-web-store.png" alt="" />
   </div>
 );
 
