d5af762b884e64455c225a614007020675d3d117
Frédéric Renken <renken@users.noreply.github.com>
2016-11-22 14:51:19 -0800

    Add edgeNgram comments back in

--- server/config/models/pageModel.js
+++ server/config/models/pageModel.js
@@ -116,3 +116,29 @@ module.exports = {
     });
   },
 };
+
+// PUT /archive
+// const edgeNgram = {
+//   settings: {
+//     number_of_shards: 1,
+//     analysis: {
+//       filter: {
+//         autocomplete_filter: {
+//           type: 'edge_ngram',
+//           min_gram: 1,
+//           max_gram: 20,
+//         },
+//       },
+//       analyzer: {
+//         autocomplete: {
+//           type: 'custom',
+//           tokenizer: 'standard',
+//           filter: [
+//             'lowercase',
+//             'autocomplete_filter',
+//           ],
+//         },
+//       },
+//     },
+//   },
+// };
