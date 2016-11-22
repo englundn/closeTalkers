3e8bca61b8b2755aab2a993148d9f4b7204d14ea
Jace <chesszhu@gmail.com>
2016-11-22 14:02:08 -0800

    (feat) sending page body from extension to avoid using diffbot

--- server/config/controllers/chrome.js
+++ server/config/controllers/chrome.js
@@ -1,36 +1,15 @@
-const request = require('request-promise');
 const Page = require('../models/pageModel');
-
-const token = process.env.DIFFBOT_TOKEN || require('../config').token;
-
-const options = (url) => {
-  return {
-    uri: 'http://api.diffbot.com/v3/article',
-    qs: { token, url },
-    json: true,
-  };
-};
+const extractor = require('unfluff');
 
 module.exports = {
   update: (req, res) => {
     const url = req.body.url;
     const userId = req.body.userId;
     const timeInfo = req.body.timeInfo;
+    const title = req.body.title;
+    const text = extractor(req.body.body, 'en').text || title;
 
-    request(options(url))
-      .then((data) => {
-        if (data.objects) {
-          const text = data.objects[0].text;
-          const title = data.objects[0].title;
-          if (text.length > 0) {
-            Page.update(url, userId, text, title, timeInfo);
-          } else {
-            Page.update(url, userId, title, title, timeInfo);
-          }
-        }
-      })
-      .catch(err => console.error(err));
-
+    Page.update(url, userId, text, title, timeInfo);
     res.end();
   },
 };
