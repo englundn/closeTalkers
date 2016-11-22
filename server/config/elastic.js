3e6d834abb1be8aba3a65defe9344384945cfd96
Frédéric Renken <renken@users.noreply.github.com>
2016-11-22 15:53:08 -0800

    Add checkTemplate function

--- server/config/elastic.js
+++ server/config/elastic.js
@@ -1,30 +1,60 @@
-module.exports = (callback) => {
-  callback();
-  // TODO: check if template exists, else create template
+const request = require('request-promise');
 
-  // PUT /archive
-  // const edgeNgram = {
-  //   settings: {
-  //     number_of_shards: 1,
-  //     analysis: {
-  //       filter: {
-  //         autocomplete_filter: {
-  //           type: 'edge_ngram',
-  //           min_gram: 1,
-  //           max_gram: 20,
-  //         },
-  //       },
-  //       analyzer: {
-  //         autocomplete: {
-  //           type: 'custom',
-  //           tokenizer: 'standard',
-  //           filter: [
-  //             'lowercase',
-  //             'autocomplete_filter',
-  //           ],
-  //         },
-  //       },
-  //     },
-  //   },
-  // };
+const user = process.env.ELASTIC_USER || require('./config').user;
+const pass = process.env.ELASTIC_PASSWORD || require('./config').password;
+
+const URL = 'http://1b4f84fecd657bad91626e9aa8f74e59.us-west-1.aws.found.io:9200';
+
+const checkTemplate = (callback) => {
+  const options = {
+    method: 'HEAD',
+    uri: `${URL}/_template/pages`,
+    auth: { user, pass },
+    json: true,
+  };
+
+  request(options)
+    .then(() => {
+      callback(true);
+    })
+    .catch(() => {
+      callback(false);
+    });
+};
+
+module.exports = (callback) => {
+  // check if template exists
+  checkTemplate((templateExists) => {
+    // if template doesn't exist create new template
+    if (!templateExists) {
+      // TODO: create template
+
+      // PUT /archive
+      // const edgeNgram = {
+      //   settings: {
+      //     number_of_shards: 1,
+      //     analysis: {
+      //       filter: {
+      //         autocomplete_filter: {
+      //           type: 'edge_ngram',
+      //           min_gram: 1,
+      //           max_gram: 20,
+      //         },
+      //       },
+      //       analyzer: {
+      //         autocomplete: {
+      //           type: 'custom',
+      //           tokenizer: 'standard',
+      //           filter: [
+      //             'lowercase',
+      //             'autocomplete_filter',
+      //           ],
+      //         },
+      //       },
+      //     },
+      //   },
+      // };
+    }
+    callback();
+  });
 };
