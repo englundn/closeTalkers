6030d2b7b94fa0d04e2856dc92c8da978fe1fd7c
Nick Englund <englundn@gmail.com>
2016-11-28 13:36:58 -0800

    Elastic tests now running.

--- server/config/models/pageModel.js
+++ server/config/models/pageModel.js
@@ -94,10 +94,13 @@ module.exports = {
   },
 
   // ========== UPDATE OR CREATE PAGE ============
-  update: (url, id, text, title, timeInfo) => {
+  update: (url, id, text, title, timeInfo, callback) => {
     // checks if article already exists
     request(searchOptions(id, null, getSum(text)))
       .then((data) => {
+        if (callback) {
+          callback(data);
+        }
         // if article exists just update time
         if (data && data.hits.total === 1) {
           request(updateOptions(id, timeInfo, data.hits.hits[0]._id))
@@ -111,6 +114,11 @@ module.exports = {
       .catch(() => {
         // if request is unsuccessful create article
         request(createOptions(url, title, id, text, timeInfo))
+          .then((data) => {
+            if (callback) {
+              callback(data);
+            }
+          })
           .catch(err => console.error(err.message));
       });
   },
--- test/elasticSpec.js
+++ test/elasticSpec.js
@@ -5,40 +5,45 @@ const Page = require('../server/config/models/pageModel');
 const body = '↵<div>↵    <h1>Example Domain</h1>↵    <p>This domain is established to be used for illustrative examples in documents. You may use this↵    domain in examples without prior coordination or asking for permission.</p>↵    <p><a href="http://www.iana.org/domains/example">More information...</a></p>↵</div>↵↵↵';
 const title = 'Example Domain';
 const url = 'http://www.example.com/';
-const userId = '1111';
+const userId = '136590';
 const timeInfo = [1480266663856, 1480266664224, 368];
 const data = { userId, url, timeInfo, title, body };
+let entryId = '';
 
 // send website to elastic db
 describe('Elastic', () => {
-  it('Should send websites to elastic', (done) => {
-    const request = new XMLHttpRequest();
-    request.open('POST', 'https://dejavu.ninja/api/chrome', true);
-    // request.open('POST', 'http://localhost:3000/api/chrome', true);
-    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    request.onreadystatechange = () => {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        expect(request.status).to.eql(200);
-        done();
-      }
-    };
-    request.send(JSON.stringify(data));
+  beforeEach((done) => {
+    setTimeout(done, 1000);
   });
-  it('Should retrieve website data from elastic', (done) => {
-    Page.search('Example', '1111', (results) => {
-      console.log(results);
+  it('Should send websites to elastic', (done) => {
+    Page.update(url, userId, body, title, timeInfo, (results) => {
+      entryId = results._id;
+      expect(results.result).to.eql('created');
       done();
     });
-  });
+  }).timeout(5000);
+  it('Should update website information on elastic', (done) => {
+    Page.update(url, userId, body, title, timeInfo, (results) => {
+      expect(results.result).to.eql(undefined);
+      done();
+    });
+  }).timeout(5000);
+  it('Should retrieve website data from elastic', (done) => {
+    Page.search('Example', userId, (results) => {
+      expect(results.hits.total).to.be.above(0);
+      done();
+    });
+  }).timeout(5000);
+  it('Should delete website data from elastic', (done) => {
+    Page.delete(userId, entryId, (results) => {
+      expect(results.result).to.eql('deleted');
+      done();
+    });
+  }).timeout(5000);
+  it('Should not retrieve deleted data', (done) => {
+    Page.search('Example', userId, (results) => {
+      expect(results.hits.total).to.eql(0);
+      done();
+    });
+  }).timeout(5000);
 });
-
-// delete website from db
-// describe('Spotify Request', () => {
-//   it('Should create a new playlist', (done) => {
-//     spotifyRequest.makeNewPlaylist(userId, accessToken, playlistName, isPlaylistPublic, (error, results) => {
-//       expect(error).to.eql(null);
-//       expect(results.type).to.eql('playlist');
-//       playlistId = results.id;
-//       done();
-//     });
-//   });
