61183259a8babf0518f4a1e80f91d897e2da0816
Nick Englund <englundn@gmail.com>
2016-11-29 16:55:28 -0800

    Finished vertical bar chart. May want to add tooltip.

---
+++ src/js/timeBarGraph.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const TimeBarGraph = ({ usage }) => {
+  // Create "Other" segment for uncommon websites
+  // let allTime = 0;
+  // let otherTime = 0;
+  // const otherArray = [];
+  const byTime = usage.map((site) => {
+    // allTime += site.totalTime;
+    return { name: site.domain, value: site.totalTime };
+  });
+  // for (let i = 0; i < byTime.length; i += 1) {
+  //   if (byTime[i].value < allTime / 50) {
+  //     otherTime += byTime[i].value;
+  //     otherArray.push(byTime.splice(i, 1)[0]);
+  //     i -= 1;
+  //   }
+  // }
+  // if (otherTime) {
+  //   byTime.push({ name: 'Other', value: otherTime });
+  // }
+
+  return (
+    <BarChart
+      width={600}
+      height={300}
+      data={byTime}
+      margin={{ top: 100, right: 30, left: 20, bottom: 5 }}
+      layout="vertical"
+    >
+      <YAxis type="category" dataKey="name" />
+      <XAxis type="number" hide="true" />
+      <Bar dataKey="value" fill="#8884d8" />
+    </BarChart>
+  );
+};
+
+export default TimeBarGraph;
