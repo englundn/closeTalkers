b33c01c41b00ae60b6e924d5bd37d84dac7fc8bf
Jace <chesszhu@gmail.com>
2016-11-30 10:07:22 -0800

    (feat) add ordering to filter

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Filters = ({ filters, filterSetting, setFilter, sorts, sortSetting, setSort }) => (
+const Filters = ({ filters, filterSetting, setFilter, sorts, sortSetting, setSort, order, orderSetting, setOrder }) => (
   <div className="filtersWrapper">
     <div className="filters">
       <div>
@@ -20,6 +20,16 @@ const Filters = ({ filters, filterSetting, setFilter, sorts, sortSetting, setSor
             key={index}
             className={(sortSetting === option ? 'chosenSetting' : '')}
             onClick={() => setSort(option)}
+          >{option}</span>
+        ))}
+      </div>
+      <div>
+        <span className="filtersTitle">Order by:</span>
+        {order.map((option, index) => (
+          <span
+            key={index}
+            className={(orderSetting === option ? 'chosenSetting' : '')}
+            onClick={() => setOrder(option)}
           >{option}</span>
         ))}
       </div>
