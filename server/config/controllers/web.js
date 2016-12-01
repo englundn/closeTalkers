ef0b14a3d695639dd4e7a6d8d8866f90fd18a857
Jace <chesszhu@gmail.com>
2016-11-30 18:28:59 -0800

    (feat) allow filtering for graphs

--- server/config/controllers/web.js
+++ server/config/controllers/web.js
@@ -1,30 +1,5 @@
 const Page = require('../models/pageModel');
 
-const parseTimeStats = (data) => {
-  let timeStats = data.hits.hits.map((entry) => {
-    const info = entry._source;
-    const matches = info.url.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
-    info.domain = matches && matches[1];
-
-    return info;
-  });
-
-  const obj = {};
-  timeStats.forEach((entry) => {
-    const domain = entry.domain;
-    obj[domain] = obj[domain] || { domain, entries: [], totalTime: 0 };
-    obj[domain].entries.push(entry);
-    obj[domain].totalTime += entry.totalTime;
-  });
-
-  timeStats = [];
-  for (const site of Object.keys(obj)) {
-    timeStats.push(obj[site]);
-  }
-  timeStats.sort((x, y) => y.totalTime - x.totalTime);
-  return timeStats;
-};
-
 module.exports = {
   search: (req, res) => {
     if (!req.user) { return; }
@@ -46,7 +21,7 @@ module.exports = {
 
   timeStats: (req, res) => {
     Page.timeStats(req.user.id, (data) => {
-      res.json(parseTimeStats(data));
+      res.json(data.hits.hits);
     });
   },
 };
--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -2,6 +2,55 @@ import React from 'react';
 import TimePieGraph from './timePieGraph';
 import TimeBarGraph from './timeBarGraph';
 
+const timeDiffChart = days => (entry) => {
+  const ms = days * 86400 * 1000;
+  const timeNow = new Date().getTime();
+  const newEntry = entry;
+  let totalTime = 0;
+  entry._source.timeInfo.forEach((triplet) => {
+    if (timeNow - triplet[1] < ms) {
+      totalTime += triplet[2];
+    }
+  });
+  newEntry._source.totalTime = totalTime;
+  return newEntry;
+};
+
+const filters = {
+  Today: timeDiffChart(1),
+  'This Week': timeDiffChart(7),
+  'This Month': timeDiffChart(30),
+  'This Year': timeDiffChart(365),
+  'All Time': data => data,
+};
+
+const parseTimeStats = (data) => {
+  let timeStats = data.map((entry) => {
+    const info = entry._source;
+    const matches = info.url.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
+    info.domain = matches && matches[1];
+
+    return info;
+  });
+
+  const obj = {};
+  timeStats.forEach((entry) => {
+    const domain = entry.domain;
+    obj[domain] = obj[domain] || { domain, entries: [], totalTime: 0 };
+    obj[domain].entries.push(entry);
+    obj[domain].totalTime += entry.totalTime;
+  });
+
+  timeStats = [];
+  for (const site of Object.keys(obj)) {
+    if (obj[site].totalTime !== 0) {
+      timeStats.push(obj[site]);
+    }
+  }
+  timeStats.sort((x, y) => y.totalTime - x.totalTime);
+  return timeStats;
+};
+
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
@@ -26,8 +75,8 @@ class Dashboard extends React.Component {
         </div>
         <div className="graph">
           {(this.state.graph === 'pie') ?
-            <TimePieGraph usage={this.props.usage} />
-            : <TimeBarGraph usage={this.props.usage} />
+            <TimePieGraph usage={parseTimeStats(this.props.usage.map(filters['Today']))} />
+            : <TimeBarGraph usage={parseTimeStats(this.props.usage.map(filters['Today']))} />
           }
         </div>
       </div>
