0a1cd81028a0386a69292a7a15682b647532ef32
Frédéric Renken <renken@users.noreply.github.com>
2016-11-30 19:16:47 -0800

    Made dashboard link a tag for consistency

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Modal = ({ showDashboard }) => (
   <div id="myModal" className="modal">
     <div className="modal-content">
-      <span onClick={showDashboard}><p>Dashboard</p></span>
+      <a onClick={showDashboard}><p>Dashboard</p></a>
       <a href="/logout"><p>Log out</p></a>
     </div>
   </div>
