4dc02a3360653b28194e3d2502225e89b3ed755c
Nick Englund <englundn@gmail.com>
2016-11-30 16:29:08 -0800

    Switched to heroku. Took out commented code

--- src/js/timeBarGraph.js
+++ src/js/timeBarGraph.js
@@ -2,25 +2,10 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis } from 'recharts';
 
 const TimeBarGraph = ({ usage }) => {
-  // Create "Other" segment for uncommon websites
-  // let allTime = 0;
-  // let otherTime = 0;
-  // const otherArray = [];
   const byTime = usage.map((site) => {
-    // allTime += site.totalTime;
     return { name: site.domain, value: site.totalTime };
   });
-  // for (let i = 0; i < byTime.length; i += 1) {
-  //   if (byTime[i].value < allTime / 50) {
-  //     otherTime += byTime[i].value;
-  //     otherArray.push(byTime.splice(i, 1)[0]);
-  //     i -= 1;
-  //   }
-  // }
-  // if (otherTime) {
-  //   byTime.push({ name: 'Other', value: otherTime });
-  // }
-
+  
   return (
     <BarChart
       width={960}
